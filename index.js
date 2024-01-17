6170d9f39fcfe5c8a86bb511cf3a83418445dd9d
Patrik Kusnir <patrikkusnir1@gmail.com>
2024-01-17 21:28:11 +0300

    photo album reedited, added bootstrap and jquery

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-window.onload = function () {
-    document.getElementById("fademe").style.opacity = 1;
-}
+// window.onload = function () {
+//     document.getElementById("fademe").style.opacity = 1;
+// }
+
+let gallery = $(".gallery")
+gallery.find("a").on("click", function () {
+    $('<div id="overlay"></div>').appendTo('body');
+    event.preventDefault();
+})
