51464c4ac83226f9a22b469daf42019bd577f24f
Patrik Kusnir <patrikkusnir1@gmail.com>
2024-06-12 13:36:09 +0300

    added menu, header to the webpage

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-// window.onload = function () {
-//     document.getElementById("fademe").style.opacity = 1;
-// }
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    imagesLoaded: true
+});
 
-let gallery = $(".gallery")
-gallery.find("a").on("click", function () {
-    $('<div id="overlay"></div>').appendTo('body');
-    event.preventDefault();
+var elem = document.querySelector('.slider');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    imagesLoaded: true,
+    prevNextButtons: false
+});
+
+// const save = document.querySelector(".save");
+const burger = document.querySelector(".burger");
+const logo = document.querySelector(".logo a");
+const menu = document.querySelector(".menu")
+const pageLeft = document.querySelector("page-left")
+
+// save.addEventListener("click", function () {
+//     this.classList.toggle("active")
+// })
+
+burger.addEventListener("click", function () {
+    this.classList.toggle("active")
+    logo.classList.toggle("active")
+    menu.classList.toggle("active")
+})
+
+$(window).on("scroll", function () {
+    let sticky = $(".page-left"),
+        scroll = $(window).scrollTop();
+    if (scroll >= 400) sticky.addClass("hide")
+    else sticky.removeClass("hide")
 })
